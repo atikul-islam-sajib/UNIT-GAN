78dcab27b2d3f8deac25d248df7a3b8199d3fdee
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-18 18:11:30 +0100

    netG prototype is added

--- src/generator.py
+++ src/generator.py
@@ -1,3 +1,34 @@
-import math
+import os
+import sys
+import torch
+import argparse
+import torch.nn as nn
 
-print(3 * (math.pow(2, 4)) + 4)
+sys.path.append("./src/")
+
+from residualBlock import ResidualBlock
+
+
+class Generator(nn.Module):
+    def __init__(self, in_channels: int = 256, sharedBlock: ResidualBlock = None):
+        super(Generator, self).__init__()
+
+        self.in_channels = in_channels
+
+        if isinstance(sharedBlock, ResidualBlock):
+            self.sharedBlock = sharedBlock
+        else:
+            raise ValueError(
+                "shared_block must be an instance of ResidualBlock".capitalize()
+            )
+
+    def forward(self, x: torch.Tensor):
+        if isinstance(x, torch.Tensor):
+            x = self.sharedBlock(x)
+            return x
+        else:
+            raise ValueError("Input should be the tensor type".capitalize())
+
+
+if __name__ == "__main__":
+    netG = Generator(in_channels=256)
