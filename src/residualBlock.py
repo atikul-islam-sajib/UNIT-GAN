d609dc0842f2b2e9af90ac3edd661788fa275efb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-18 12:18:04 +0100

    residualblock is added

--- src/residualBlock.py
+++ src/residualBlock.py
@@ -15,17 +15,36 @@ class ResidualBlock(nn.Module):
 
         self.reflectionpad2d = 1
         self.kernel_size = 3
-        self.stride_size = 2
-        self.padding_size = 1
+        self.stride_size = 1
 
         self.layers = []
 
+        for index in range(2):
+            self.layers.append(nn.ReflectionPad2d(self.reflectionpad2d))
+
+            self.layers.append(
+                nn.Conv2d(
+                    in_channels=self.in_channels,
+                    out_channels=self.out_channels,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride_size,
+                )
+            )
+
+            self.layers.append(nn.InstanceNorm2d(num_features=self.out_channels))
+
+            if index != 1:
+                self.layers.append(nn.ReLU())
+
+        self.residualBlock = nn.Sequential(*self.layers)
+
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
-            pass
+            return x + self.residualBlock(x)
         else:
             raise ValueError("Input should be the tensor type".capitalize())
 
 
 if __name__ == "__main__":
-    residual = ResidualBlock(in_channels=64)
+    residual = ResidualBlock(in_channels=256)
+    print(residual(torch.randn(1, 256, 32, 32)).size())
