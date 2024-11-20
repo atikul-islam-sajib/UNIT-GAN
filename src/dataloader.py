3159358e17f2b8805996e6f35c1361744b8af255
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-20 12:31:09 +0100

    unzipping is completed

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,20 +1,28 @@
 import os
-impirt sys
+import zipfile
+import sys
 import torch
 import argparse
 import torch.nn as nn
 
 sys.path.append("./src/")
 
+from utils import config
+
 class Loader():
-    def __int__(self, dataset: str = None, image_size: int = 128, batch_size: int = 1, split_size: float = 0.20):
+    def __init__(self, dataset=None, image_size: int = 128, batch_size: int = 1, split_size: float = 0.20):
         self.dataset = dataset
         self.image_size = image_size
         self.batch_size = batch_size
         self.split_size = split_size
         
     def unzip_folder(self):
-        pass
+        os.makedirs(config()["path"]["processed_path"], exist_ok=True)
+        
+        with zipfile.ZipFile(self.dataset, mode='r') as zip_ref:
+            zip_ref.extractall(path=config()["path"]["processed_path"])
+            
+        print(f"""Unzip folder {config()["path"]["processed_path"]}""".capitalize())
     
     def split_dataset(self):
         pass
@@ -32,6 +40,6 @@ class Loader():
     def dataset_details():
         pass
     
-    
 if __name__ == "__main__":
-    pass
+    loader = Loader(dataset="./data/raw/dataset.zip")
+    loader.unzip_folder()
