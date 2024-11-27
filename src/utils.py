16f7450a7c104580567ebb2d008291c79a6d0347
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-27 17:27:28 +0100

    device init is done

--- src/utils.py
+++ src/utils.py
@@ -25,3 +25,12 @@ def load(filename=None):
 def config():
     with open("./config.yml", "r") as file:
         return yaml.safe_load(file)
+
+
+def device_init(device: str = "cuda"):
+    if device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    elif device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
