bf3122422921cb97a332cc18318db6dbbca95407
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-12-01 21:12:57 +0100

    trainer prototype is added with init

--- src/trainer.py
+++ src/trainer.py
@@ -2,11 +2,18 @@ import os
 import sys
 import torch
 import argparse
+import torch.optim as optim
 import torch.nn as nn
 
 sys.path.append("./src/")
 
 from helper import helper
+from encoder import Encoder
+from gan_loss import GANLoss
+from generator import Generator
+from pixel_loss import PixelLoss
+from kl_divergence import KLDivergence
+from discriminator import Discriminator
 from utils import config, dump, load, device_init, weight_init
 
 
@@ -49,3 +56,75 @@ class Trainer:
             adam=self.adam,
             SGD=self.SGD,
         )
+
+        self.train_dataloader = self.init["train_dataloader"]
+        self.valid_dataloader = self.init["valid_dataloader"]
+
+        self.encoder1 = self.init["E1"]
+        self.encoder2 = self.init["E2"]
+
+        self.netG1 = self.init["netG1"]
+        self.netG2 = self.init["netG2"]
+
+        self.netD1 = self.init["netD1"]
+        self.netD2 = self.init["netD2"]
+
+        self.optimizerG = self.init["optimizerG"]
+        self.optimizerD1 = self.init["optimizerD1"]
+        self.optimizerD2 = self.init["optimizerD2"]
+
+        self.criterion = self.init["criterion"]
+        self.pixelLoss = self.init["pixelLoss"]
+        self.kl_loss = self.init["kl_loss"]
+
+        assert (
+            self.train_dataloader.__class__ == torch.utils.data.DataLoader
+        ), "Train dataloader shoould be torch.utils.data.DataLoader".capitalize()
+        assert (
+            self.valid_dataloader.__class__ == torch.utils.data.DataLoader
+        ), "Valid dataloader shoould be torch.utils.data.DataLoader".capitalize()
+
+        assert (
+            self.encoder1.__class__ == Encoder
+        ), "Encoder object should be Encoder class".capitalize()
+        assert (
+            self.encoder2.__class__ == Encoder
+        ), "Encoder object should be Encoder class".capitalize()
+
+        assert (
+            self.netG1.__class__ == Generator
+        ), "Generator object should be Generator class".capitalize()
+        assert (
+            self.netG2.__class__ == Generator
+        ), "Generator object should be Generator class".capitalize()
+
+        assert (
+            self.netD1.__class__ == Discriminator
+        ), "netD1 object should be Discriminator class".capitalize()
+        assert (
+            self.netD2.__class__ == Discriminator
+        ), "netD2 object should be Discriminator class".capitalize()
+
+        assert (
+            self.optimizerG.__class__ == optim.Adam
+        ), "optimizerG object should be Adam class".capitalize()
+        assert (
+            self.optimizerD1.__class__ == optim.Adam
+        ), "optimizerD1 object should be Adam class".capitalize()
+        assert (
+            self.optimizerD2.__class__ == optim.Adam
+        ), "optimizerD2 object should be Adam class".capitalize()
+
+        assert (
+            self.criterion.__class__ == GANLoss
+        ), "Criterion object should be GANLoss class".capitalize()
+        assert (
+            self.pixelLoss.__class__ == PixelLoss
+        ), "pixelLoss object should be PixelLoss class".capitalize()
+        assert (
+            self.kl_loss.__class__ == KLDivergence
+        ), "KL Divergence object should be PixelLoss class".capitalize()
+        
+        
+if __name__ == "__main__":
+    trainer = Trainer()
